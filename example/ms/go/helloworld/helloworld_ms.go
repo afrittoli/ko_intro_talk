1459bfa73f4242e6db4ea8a263c1d2551cec6406
Andrea Frittoli <andrea.frittoli@gmail.com>
2019-05-21 08:23:47 +0200

    Add reading kodata to the demo

--- example/ms/go/helloworld/helloworld_ms.go
+++ example/ms/go/helloworld/helloworld_ms.go
@@ -3,17 +3,32 @@ package main
 import (
   "flag"
   "fmt"
+  "io/ioutil"
+  "log"
   "net/http"
+  "os"
+  "path/filepath"
   "strings"
 )
+
+func loadGreeting() string {
+  dp := os.Getenv("KO_DATA_PATH")
+  file := filepath.Join(dp, "static_greet")
+  bytes, err := ioutil.ReadFile(file)
+  if err != nil {
+    log.Fatalf("Error reading %q: %v", file, err)
+  }
+  return string(bytes)
+}
 
 func main() {
   hwPort := flag.Int("port", 8080, "Listening port numbers")
   flag.Parse()
+  greetingPrefix := loadGreeting()
 
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     recipient := strings.Replace(r.URL.Path, "/", "", -1)
-    fmt.Fprintf(w, "{\"hello\": \"%s\"}", recipient)
+    fmt.Fprintf(w, "{\"%s hello\": \"%s\"}\n", greetingPrefix, recipient)
   })
 
   http.ListenAndServe(fmt.Sprintf(":%d", *hwPort), nil)
